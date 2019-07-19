2ea43c982c26e600096f10f4f91960592456fc63
Fanni Szikszai <fanniszikszai@gmail.com>
2019-07-19 11:02:15 +0200

    throw error implemented

--- src/js/index.js
+++ src/js/index.js
@@ -1,56 +1,65 @@
 import { getBooks } from './models/search';
 import { elements } from './models/base';
-import { clearInputAll, getInputs, renderSpinner, clearSpinner} from './views/searchView';
+import {
+  clearInputAll,
+  getInputs,
+  renderSpinner,
+  clearSpinner
+} from './views/searchView';
 import * as resView from './views/resultsView';
 
 let books;
 
 // CONTROLLER
 const controlSearch = async () => {
-    // 1. get the queries
-    const input = getInputs();
-    // 2. prepare UI for the changes
-    clearInputAll(); // clear the input fields
-    resView.clearResults(); // clear the previous results
-    resView.clearBtnAndInfo(); // clear the pagination buttons and page info text
-    renderSpinner(); // render the loading spinner
+  // 1. get the queries
+  const input = getInputs();
+  // 2. prepare UI for the changes
+  clearInputAll(); // clear the input fields
+  resView.clearResults(); // clear the previous results
+  resView.clearBtnAndInfo(); // clear the pagination buttons and page info text
+  renderSpinner(); // render the loading spinner
 
-    if (input.query) {
-        // 3. search for the queries
-        books = await getBooks(input.query, input.author, input.isbn, input.lang);
-        // 4. render the result to the UI
-        clearSpinner();
-        resView.renderResults(books);
+  if (input.query) {
+    try {
+      // 3. search for the queries
+      books = await getBooks(input.query, input.author, input.isbn, input.lang);
+      // 4. render the result to the UI
+      clearSpinner();
+      resView.renderResults(books);
+    } catch (err) {
+      console.log(err);
+      alert('Oops, something went wrong. Please try again later.');
     }
+  }
 };
 
 elements.searchBtn.addEventListener('click', e => {
-    e.preventDefault();
-    controlSearch();
+  e.preventDefault();
+  controlSearch();
 });
 
 document.addEventListener('keypress', e => {
-    if (event.keyCode === 13 || event.which === 13) {
-        e.preventDefault();
-        controlSearch();
-    }
+  if (event.keyCode === 13 || event.which === 13) {
+    e.preventDefault();
+    controlSearch();
+  }
 });
 
 // PAGINATION CONTROLLER
 elements.sectionPages.addEventListener('click', e => {
-    const btnPrev = e.target.closest('.btn--prev');
-    const btnNext = e.target.closest('.btn--next');
-    if (btnPrev) {
-        const goToPrevPage = parseInt(btnPrev.dataset.goto, 10);
-        resView.clearResults();
-        resView.clearBtnAndInfo();
-        resView.renderResults(books, goToPrevPage);
-    }
-    if (btnNext) {
-        const goToNextPage = parseInt(btnNext.dataset.goto, 10);
-        resView.clearResults();
-        resView.clearBtnAndInfo();
-        resView.renderResults(books, goToNextPage);
-    }
+  const btnPrev = e.target.closest('.btn--prev');
+  const btnNext = e.target.closest('.btn--next');
+  if (btnPrev) {
+    const goToPrevPage = parseInt(btnPrev.dataset.goto, 10);
+    resView.clearResults();
+    resView.clearBtnAndInfo();
+    resView.renderResults(books, goToPrevPage);
+  }
+  if (btnNext) {
+    const goToNextPage = parseInt(btnNext.dataset.goto, 10);
+    resView.clearResults();
+    resView.clearBtnAndInfo();
+    resView.renderResults(books, goToNextPage);
+  }
 });
-
--- src/js/models/search.js
+++ src/js/models/search.js
@@ -2,12 +2,13 @@ import axios from 'axios';
 import { apiKey } from '../config';
 
 export const getBooks = async (query, author, isbn, lang) => {
-    try {
-        const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?&key=${apiKey}&q=${query}+inauthor:${author}&isbn:${isbn}&langRestrict=${lang}&maxResults=20&printType=books`);
-        const itemsArr = response.data.items;
-        return itemsArr;
-    } catch(e) {
-        console.log(e.response);
-        alert('Oops, something went wrong. Please try again later.');
-    }
+  try {
+    const response = await axios.get(
+      `https://www.googleapis.com/books/v1/volumes?&key=${apiKey}&q=${query}+inauthor:${author}&isbn:${isbn}&langRestrict=${lang}&maxResults=20&printType=books`
+    );
+    const itemsArr = response.data.items;
+    return itemsArr;
+  } catch (err) {
+    throw err;
+  }
 };
